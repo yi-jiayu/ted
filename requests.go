efdd5fbec469c902768aef085e90716ef9393964
Jiayu Yi <yijiayu@gmail.com>
2020-03-17 13:02:09 +0800

    Add EditMessageTextRequest

--- requests.go
+++ requests.go
@@ -50,3 +50,33 @@ type AnswerCallbackQueryRequest struct {
 func (r AnswerCallbackQueryRequest) doWith(bot Bot) (Response, error) {
 	return bot.doJSON("answerCallbackQuery", r)
 }
+
+type EditMessageTextRequest struct {
+	// ChatID is a string when it refers to the username of a channel and an integer otherwise.
+	// Required if InlineMessageID is not specified.
+	ChatID interface{} `json:"chat_id,omitempty"`
+
+	// MessageID is required when InlineMessageID is not specified.
+	MessageID int `json:"message_id,omitempty"`
+
+	// InlineMessageID is required when ChatID and MessageID are not specified.
+	InlineMessageID string `json:"inline_message_id,omitempty"`
+
+	// Text is the new text of the message. It should be limited to 1-4096 characters after entities parsing.
+	Text string `json:"text"`
+
+	// ParseMode can be specified to show bold, italic, fixed-width text or inline URLs in messages.
+	// Possible values are "Markdown", "MarkdownV2" and "HTML". Refer to
+	// https://core.telegram.org/bots/api#formatting-options for more information.
+	ParseMode string `json:"parse_mode,omitempty"`
+
+	// DisableWebPagePreview will disable link previews for links in this message.
+	DisableWebPagePreview bool `json:"disable_web_page_preview,omitempty"`
+
+	// ReplyMarkup can be provided to display an inline keyboard with the updated message.
+	ReplyMarkup *InlineKeyboardMarkup `json:"reply_markup,omitempty"`
+}
+
+func (e EditMessageTextRequest) doWith(bot Bot) (Response, error) {
+	return bot.doJSON("editMessageText", e)
+}
