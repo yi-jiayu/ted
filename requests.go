b23083958d09fbf4d2e8ffe89803a60f8ba8f39c
Jiayu Yi <yijiayu@gmail.com>
2020-03-18 14:42:16 +0800

    Add ForceReply type

--- requests.go
+++ requests.go
@@ -38,10 +38,39 @@ type ReplyMarkup interface {
 	replyMarkup()
 }
 
+// Upon receiving a message with this object, Telegram clients will display a
+// reply interface to the user (act as if the user has selected the bot‘s
+// message and tapped ’Reply'). This can be extremely useful if you want to
+// create user-friendly step-by-step interfaces without having to sacrifice
+// privacy mode.
+type ForceReply struct {
+	// Use this parameter if you want to force reply from specific users
+	// only. Targets: 1) users that are @mentioned in the text of the
+	// Message object; 2) if the bot's message is a reply (has
+	// reply_to_message_id), sender of the original message.
+	Selective bool
+}
+
+func (f ForceReply) replyMarkup() {}
+
+func (f ForceReply) MarshalJSON() ([]byte, error) {
+	data, err := json.Marshal(struct {
+		ForceReply bool `json:"force_reply"`
+		Selective  bool `json:"selective,omitempty"`
+	}{
+		ForceReply: true,
+		Selective:  f.Selective,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(string(data))
+}
+
 type InlineKeyboardButton struct {
 	Text         string `json:"text"`
-	URL          string `json:"url"`
-	CallbackData string `json:"callback_data"`
+	URL          string `json:"url,omitempty"`
+	CallbackData string `json:"callback_data,omitempty"`
 }
 
 type InlineKeyboardMarkup struct {
--- requests_test.go
+++ requests_test.go
@@ -20,5 +20,18 @@ func TestInlineKeyboardMarkup_MarshalJSON(t *testing.T) {
 	}
 	actual, err := json.Marshal(markup)
 	assert.NoError(t, err)
-	assert.Equal(t, `"{\"inline_keyboard\":[[{\"text\":\"Text\",\"url\":\"\",\"callback_data\":\"Data\"}]]}"`, string(actual))
+	assert.Equal(t, `"{\"inline_keyboard\":[[{\"text\":\"Text\",\"callback_data\":\"Data\"}]]}"`, string(actual))
+}
+
+func TestForceReply_MarshalJSON(t *testing.T) {
+	var JSON []byte
+	var err error
+	JSON, err = json.Marshal(ForceReply{})
+	assert.NoError(t, err)
+	assert.Equal(t, `"{\"force_reply\":true}"`, string(JSON))
+	JSON, err = json.Marshal(ForceReply{
+		Selective: true,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, `"{\"force_reply\":true,\"selective\":true}"`, string(JSON))
 }
