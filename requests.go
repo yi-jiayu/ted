70f04e8b42e668b8a2770139efb5f8f955855ebb
Jiayu Yi <yijiayu@gmail.com>
2020-03-20 23:41:51 +0800

    Fix wrong type in answerCallbackQuery

--- requests.go
+++ requests.go
@@ -184,12 +184,34 @@ func (i InlineKeyboardMarkup) MarshalJSON() ([]byte, error) {
 	return json.Marshal(string(data))
 }
 
+// Use this method to send answers to callback queries sent from inline
+// keyboards. The answer will be displayed to the user as a notification at the
+// top of the chat screen or as an alert. On success, True is returned.
 type AnswerCallbackQueryRequest struct {
+	// Unique identifier for the query to be answered
 	CallbackQueryID string `json:"callback_query_id"`
-	Text            string `json:"text,omitempty"`
-	ShowAlert       bool   `json:"show_alert,omitempty"`
-	URL             string `json:"url,omitempty"`
-	CacheTime       string `json:"cache_time,omitempty"`
+
+	// Text of the notification. If not specified, nothing will be shown to
+	// the user, 0-200 characters
+	Text string `json:"text,omitempty"`
+
+	// If true, an alert will be shown by the client instead of a
+	// notification at the top of the chat screen. Defaults to false.
+	ShowAlert bool `json:"show_alert,omitempty"`
+
+	// 	URL that will be opened by the user's client. If you have
+	// 	created a Game and accepted the conditions via @Botfather,
+	// 	specify the URL that opens your game – note that this will only
+	// 	work if the query comes from a callback_game button.
+	//
+	// Otherwise, you may use links like t.me/your_bot?start=XXXX that open
+	// your bot with a parameter.
+	URL string `json:"url,omitempty"`
+
+	// 	The maximum amount of time in seconds that the result of the
+	// 	callback query may be cached client-side. Telegram apps will
+	// 	support caching starting in version 3.14. Defaults to 0.
+	CacheTime int `json:"cache_time,omitempty"`
 }
 
 func (r AnswerCallbackQueryRequest) doWith(bot Bot) (Response, error) {
