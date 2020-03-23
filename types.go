8c70956c95ceb801202ab796ecd772d7e0bc385b
Jiayu Yi <yijiayu@gmail.com>
2020-03-23 19:48:03 +0800

    Add ChosenInlineResult type

--- types.go
+++ types.go
@@ -87,3 +87,23 @@ type InlineQuery struct {
 	Query    string    `json:"query"`
 	Offset   string    `json:"offset"`
 }
+
+// ChosenInlineResult represents a result of an inline query that was chosen by the user and sent to their chat partner.
+//
+// Note: It is necessary to enable inline feedback via @Botfather in order to receive these objects in updates.
+type ChosenInlineResult struct {
+	// The unique identifier for the result that was chosen
+	ID string `json:"id"`
+
+	// The user that chose the result
+	From User `json:"from"`
+
+	// Optional. Sender location, only for bots that require user location
+	Location Location `json:"location"`
+
+	// Optional. Identifier of the sent inline message. Available only if there is an inline keyboard attached to the message. Will be also received in callback queries and can be used to edit the message.
+	InlineMessageID string `json:"inline_message_id"`
+
+	// The query that was used to obtain the result
+	Query string `json:"query"`
+}
