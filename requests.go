dec0d14a5ab1252449aa92102d917a0244885d21
Jiayu Yi <yijiayu@gmail.com>
2020-03-31 14:44:44 +0800

    Implement setWebhook

--- requests.go
+++ requests.go
@@ -10,6 +10,34 @@ func (g GetMeRequest) doWith(bot Bot) (Response, error) {
 	return bot.doQuery("getMe", nil)
 }
 
+type SetWebhookRequest struct {
+	// URL is a HTTPS URL to send updates to. Use an empty string to remove
+	// webhook integration
+	URL string `json:"url"`
+
+	// MaxConnections is the maximum allowed number of simultaneous HTTPS
+	// connections to the webhook for update delivery, 1-100. Defaults to
+	// 40. Use lower values to limit the load on your bot‘s server, and
+	// higher values to increase your bot’s throughput.
+	MaxConnections int `json:"max_connections,omitempty"`
+
+	// AllowedUpdates is a JSON-serialized list of the update types you
+	// want your bot to receive. For example, specify ["message",
+	// "edited_channel_post", "callback_query"] to only receive updates of
+	// these types. See Update for a complete list of available update
+	// types. Specify an empty list to receive all updates regardless of
+	// type (default). If not specified, the previous setting will be used.
+	//
+	// Please note that this parameter doesn't affect updates created
+	// before the call to the setWebhook, so unwanted updates may be
+	// received for a short period of time.¬
+	AllowedUpdates []string `json:"allowed_updates,omitempty"`
+}
+
+func (s SetWebhookRequest) doWith(bot Bot) (Response, error) {
+	return bot.doJSON("setWebhook", s)
+}
+
 type SendMessageRequest struct {
 	// ChatID is a string when it refers to the username of a channel and an integer otherwise.
 	// Required if InlineMessageID is not specified.
